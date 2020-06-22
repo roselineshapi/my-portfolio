98711bb6280b2e7980e1ba03a0ccd7a0d27a668b
roselineshapi <roseshapi81@gmail.com>
2020-06-22 16:40:42 -0400

    removed random function

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -12,25 +12,11 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-/**
- * Adds a random greeting to the page.
- */
-function addRandomGreeting() {
-  const greetings =
-      ['Hello world!', '¡Hola Mundo!', '你好，世界！', 'Bonjour le monde!'];
-
-  // Pick a random greeting.
-  const greeting = greetings[Math.floor(Math.random() * greetings.length)];
-
-  // Add it to the page.
-  const greetingContainer = document.getElementById('greeting-container');
-  greetingContainer.innerText = greeting;
-}
 /* Function to return data */
 async function getDataUsingAsyncAwait() {
-  const response = await fetch('/data');
-  const data = await response.text();
-  document.getElementById('data-container').innerText = data;
+    const response = await fetch('/data');
+    const data = await response.text();
+    document.getElementById('data-container').innerText = data;
 }
 
 function getMessagesJSON(){
